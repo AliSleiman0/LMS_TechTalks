369b0359b1e6a039dea679b1f228e243c001cced
Zouheir AL Sayyed <zouheirsayyed2@gmail.com>
2025-07-22 01:06:35 +0300

    Updated The Interface and the implementation

--- Backend/LMS_Backend.Service/Services/UserInterface.cs
+++ Backend/LMS_Backend.Service/Services/UserInterface.cs
@@ -2,55 +2,58 @@
 using System.Threading.Tasks;
 using LMS_Backend.Domain.Models;
 using LMS_Backend.Service.Interfaces;
+using LMS_Infrastructure.Context;
 using LMS_Infrastructure.Repository;
 
-namespace LMS_Backend.Service.Services
-{
-    public class UserInterface : IUserInterface
-    {
-        private readonly IRepository<User> _userRepository;
+
+// THIS MUST BE IMPLEMENTED AS A SERVICE TO FOLLOW THE SOLID PRINCIPLES AND SEPARATION OF CONCERNS WHERE WE ALREADY HAVE THE GENERIC REPO , SO IMPLEMENTING SERVICES OF GET ALL , GET BY ID  AND DELETE AND THOSE ARE DONE IN SERVICES WHICH WILL BE USED IN APIs 
+// SO WE DONT NEED TO REWRITE THE SAME CODE IN THE CONTROLLER OR API ENDPOINTS AND SO NO NEED TO THIS FILE THUS I WILL COMMENT IT OUT FOR NOW TILL YOU REVIEW IT ALI 
 
 
-        public UserInterface(IRepository<User> userRepository)
-        {
-            _userRepository = userRepository;
-        }
+//namespace LMS_Backend.Service.Services
+//{
 
-        public async Task<IEnumerable<User>> GetAllUsersAsync()
-        {
-            return await _userRepository.GetAllAsync();
-        }
 
-        public async Task<User?> GetUserByIdAsync(int id)
-        {
-            return await _userRepository.GetByIdAsync(id);
-        }
 
-        public async Task AddUserAsync(User user)
-        {
-            await _userRepository.AddAsync(user);
-        }
 
-        public async Task UpdateUserAsync(User user)
-        {
-            await _userRepository.UpdateAsync(user);
-        }
 
-        public async Task DeleteUserAsync(int id)
-        {
-            await _userRepository.DeleteAsync(id);
-        }
 
-        Task<IEnumerable<User>> IUserInterface.GetAllUsersAsync()
-        {
-            throw new NotImplementedException();
-        }
 
-        Task<User?> IUserInterface.GetUserByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
 
-       
-    }
-}
+//public class UserInterface : Repository<User> ,IUserInterface
+//{
+//	public UserInterface(DBContext context) : base(context) { }
+
+//public async Task<IEnumerable<User>> GetAllUsersAsync()
+//      {
+//          return await _userRepository.GetAllAsync();
+//      }
+
+//      public async Task<User?> GetUserByIdAsync(Guid id)
+//      {
+//          return await _userRepository.GetByIdAsync(id);
+//      }
+
+//      public async Task AddUserAsync(User user)
+//      {
+//          await _userRepository.AddAsync(user);
+//      }
+
+//      public async Task UpdateUserAsync(User user)
+//      {
+//          await _userRepository.UpdateAsync(user);
+//      }
+
+//      public async Task DeleteUserAsync(Guid id)
+//      {
+//          var user = await _userRepository.GetByIdAsync(id);
+//	await _userRepository.DeleteAsync(user);
+//      }
+
+
+
+
+
+
+//    }
+//}
