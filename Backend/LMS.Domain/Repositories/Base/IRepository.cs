eb59dfecb3c46d3eff582c6c9694ef53e78c5add
Zouheir AL Sayyed <zouheirsayyed2@gmail.com>
2025-07-22 01:07:05 +0300

    Updated the IRepo ( Generic )

---
+++ Backend/LMS.Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using Academia.Core.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LMS_Infrastructure.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+		Task<IReadOnlyList<T>> GetAllAsync();
+
+		Task<PaginatedResult<T>> GetAllWithPaginationAsync(
+		int pageIndex,
+		int pageSize,
+		Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+		List<Expression<Func<T, object>>> includes = null,
+		bool disableTracking = true);
+
+
+		Task<T> GetByIdAsync(Guid id);
+
+		Task<IReadOnlyList<T>> GetAsync(
+			Expression<Func<T, bool>> predicate);
+
+		Task<IReadOnlyList<T>> GetAsync(
+			Expression<Func<T, bool>> predicate = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+			string includeString = null,
+			bool disableTracking = true);
+
+		Task<IReadOnlyList<T>> GetAsync(
+			Expression<Func<T, bool>> predicate = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+			List<Expression<Func<T, object>>> includes = null,
+			bool disableTracking = true);
+
+		Task<T> AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+	}
+}
