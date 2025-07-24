e11b1bcc07608d5dd87f5bd7dd0166f66c829f95
Mhmdzein1 <mohammad_zein_2000@hotmail.com>
2025-07-24 21:28:47 +0300

    router
    new pages

--- Frontend/frontend_react/src/Router/AppRouter.tsx
+++ Frontend/frontend_react/src/Router/AppRouter.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from '../Pages/MainPage';
+import LoginPage from '../Pages/Login-Page';
+
+import  SignUpPage from '../Pages/SignUp-Page';
+
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+                <Route path="/signin" element={<LoginPage/>} />
+                        <Route path="/signup" element={<SignUpPage />} />
+
+
       </Routes>
     </Router>
   );
