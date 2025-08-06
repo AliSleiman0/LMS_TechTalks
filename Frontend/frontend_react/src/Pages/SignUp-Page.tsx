808488111396308807cfede495d1859e244b8e20
Mhmdzein1 <mohammad_zein_2000@hotmail.com>
2025-08-06 21:50:23 +0300

    Update signup

--- Frontend/frontend_react/src/Pages/SignUp-Page.tsx
+++ Frontend/frontend_react/src/Pages/SignUp-Page.tsx
@@ -1,23 +1,43 @@
-import "../Pages-Styles/SignUp-Page.css"
+import "../Pages-Styles/SignUp-Page.css";
 const SignUpPage: React.FC = () => {
- 
   return (
     <div className="Signup-Container">
-   <div className="Signup-main">
-    <h1 className="Signup-h1">Sign Up</h1>
-    <form className="Signup-form ">
-        <input type="text" name="firstname" placeholder="First Name" required />
-        <input type="text" name="lastname" placeholder="Last Name" required />
+      <div className="Signup-main">
+        <h1 className="Signup-h1">Sign Up</h1>
+        <form className="Signup-form ">
+          <input type="text" name="fullname" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
 
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <button className="Signup-button" type="submit">Sign Up</button>
-    </form>
-    <div className="login-message">
-        <p>Already have an account? <a href="/signin" className="login-link">Sign In</a></p>
+          <h5>Select Role:</h5>
+          <label>
+            <input type="radio" name="accountType" value="personal" required />
+            Instructor
+          </label>
+          <label>
+            <input type="radio" name="accountType" value="business" required />
+            Student
+          </label>
+          <button className="Signup-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+
+        <div className="login-message">
+          <p>
+            Already have an account?{" "}
+            <a href="/signin" className="login-link">
+              Sign In
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-</div>
-</div>
   );
 };
 
