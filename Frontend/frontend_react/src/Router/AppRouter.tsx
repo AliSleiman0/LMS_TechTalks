1f22588ef8348da66342bed46c95f81df343feab
Ali Sleiman <asleiman@acksession.com>
2025-08-06 14:13:57 +0300

    CourseDetail Page

--- Frontend/frontend_react/src/Router/AppRouter.tsx
+++ Frontend/frontend_react/src/Router/AppRouter.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "../Pages/MainPage";
+
 import LoginPage from "../Pages/Login-Page";
 import SignUpPage from "../Pages/SignUp-Page";
 import Layout from "../Components/Layout/Layout";
 import FeaturedCourses from "../Pages/Featured-Courses-Page";
 import LandingPage from "../Pages/LandingPage";
+// Add this import
+import CourseDetailPage from "../Pages/CourseDetailPage"; // adjust path if needed
 
 const AppRouter: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* Layout wrapper */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
-          <Route path="/FCourses" element={<FeaturedCourses />} />
-        </Route>
+    return (
+        <Router>
+            <Routes>
+                {/* Layout wrapper */}
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<LandingPage />} />
+                    <Route path="/courses/:courseId" element={<CourseDetailPage />} />
 
-        {/* Outside layout */}
-        <Route path="/signin" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </Router>
-  );
+                    <Route path="/FCourses" element={<FeaturedCourses />} />
+                </Route>
+
+                {/* Outside layout */}
+                <Route path="/signin" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRouter;
